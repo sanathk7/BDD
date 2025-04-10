0c79fda0188ddd9af48ec32198bf03ca74fb4e4e
sanathk7 <122560253+sanathk7@users.noreply.github.com>
2025-04-10 12:29:35 +0530

    Update ReqMethod.java

--- src/test/java/requestmethod/ReqMethod.java
+++ src/test/java/requestmethod/ReqMethod.java
@@ -41,7 +41,7 @@ public static Response sendRequest(String request,String endpoint,String BaseUrl
             throw new RuntimeException("Failed to read JSON file", e);
         }	*/
         try {
-             create_data= new String(Files.readAllBytes(Paths.get("C:\\Users\\sakum\\eclipse-workspace\\BDD_Restassured\\src\\test\\java\\userdata\\inputdata.json" )));
+             create_data= new String(Files.readAllBytes(Paths.get("src\\test\\java\\userdata\\inputdata.json" )));
         } catch (IOException e) {
         	 throw new RuntimeException("Failed to read JSON file", e);
         }
@@ -65,7 +65,7 @@ public static Response sendRequest(String request,String endpoint,String BaseUrl
     	System.out.println("put"+endpoint+UserId.id); 
     	try {
             update_data= new String(Files.readAllBytes(Paths.get(
-               "C:\\Users\\sakum\\eclipse-workspace\\BDD_Restassured\\src\\test\\java\\userdata\\updatedata.json" )));
+               "src\\test\\java\\userdata\\updatedata.json" )));
        } catch (IOException e) {
        	 throw new RuntimeException("Failed to read JSON file", e);
        }
