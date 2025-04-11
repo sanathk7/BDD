1435687c72b87aa318fcaf5caae37d730bcfa32a
sanathk7 <122560253+sanathk7@users.noreply.github.com>
2025-04-11 15:38:44 +0530

    Update TestRunnerTest.java

--- src/test/java/runners/TestRunnerTest.java
+++ src/test/java/runners/TestRunnerTest.java
@@ -2,14 +2,18 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
 
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/Feature", // Corrected path
     glue = {"Stepdefination", "utils"},
     plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports.json"},
     monochrome = true
 )
-public class TestRunnerTest extends AbstractTestNGCucumberTests {
+public class TestRunnerTest{
 }
 
 
